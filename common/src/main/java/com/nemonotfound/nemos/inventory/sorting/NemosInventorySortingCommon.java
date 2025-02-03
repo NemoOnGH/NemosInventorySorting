d1ec2d42b08a548072c6b554758aa39a9eb708e0
NemoNotFound <02grimes-bard@icloud.com>
2025-02-03 19:16:39 +0100

    Update 1.7.2

--- common/src/main/java/com/nemonotfound/nemos/inventory/sorting/NemosInventorySortingCommon.java
+++ common/src/main/java/com/nemonotfound/nemos/inventory/sorting/NemosInventorySortingCommon.java
@@ -1,6 +1,5 @@
 package com.nemonotfound.nemos.inventory.sorting;
 
-import com.nemonotfound.nemos.inventory.sorting.client.ModKeyMappings;
 import com.nemonotfound.nemos.inventory.sorting.platform.RegistryHelper;
 
 import java.util.ServiceLoader;
@@ -11,7 +10,5 @@ public class NemosInventorySortingCommon {
 
     public static void init() {
         Constants.LOG.info("Thank you for using Nemo's Inventory Sorting!");
-
-        ModKeyMappings.init();
     }
 }
