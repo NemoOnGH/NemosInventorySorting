376860ff1acc6359830a1f8fd923fc1abea14052
NemoNotFound <02grimes-bard@icloud.com>
2025-03-05 20:26:09 +0100

    Update 1.8.1

--- neoforge/src/main/java/com/nemonotfound/nemos/inventory/sorting/NemosInventorySortingNeoForgeClient.java
+++ neoforge/src/main/java/com/nemonotfound/nemos/inventory/sorting/NemosInventorySortingNeoForgeClient.java
@@ -2,11 +2,22 @@ package com.nemonotfound.nemos.inventory.sorting;
 
 
 import com.nemonotfound.nemos.inventory.sorting.client.ModKeyMappings;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.packs.PackLocationInfo;
+import net.minecraft.server.packs.PackSelectionConfig;
+import net.minecraft.server.packs.PackType;
+import net.minecraft.server.packs.PathPackResources;
+import net.minecraft.server.packs.repository.Pack;
+import net.minecraft.server.packs.repository.PackSource;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModList;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
+import net.neoforged.neoforge.event.AddPackFindersEvent;
+
+import java.util.Optional;
 
 import static com.nemonotfound.nemos.inventory.sorting.Constants.MOD_ID;
 
@@ -15,6 +26,7 @@ import static com.nemonotfound.nemos.inventory.sorting.Constants.MOD_ID;
 public class NemosInventorySortingNeoForgeClient {
 
     public NemosInventorySortingNeoForgeClient() {
+        NemosInventorySortingClientCommon.init();
     }
 
     @SubscribeEvent
@@ -29,5 +41,25 @@ public class NemosInventorySortingNeoForgeClient {
         event.register(ModKeyMappings.SORT_ALPHABETICALLY_INVENTORY.get());
         event.register(ModKeyMappings.SORT_ALPHABETICALLY_DESCENDING.get());
         event.register(ModKeyMappings.SORT_ALPHABETICALLY_DESCENDING_INVENTORY.get());
+    }
+
+    @SubscribeEvent
+    public static void addBuiltInResourcePack(AddPackFindersEvent event) {
+        if (event.getPackType() == PackType.CLIENT_RESOURCES) {
+            var resourcePath = ModList.get().getModFileById(MOD_ID).getFile().findResource("resourcepacks/dark_mode");
+            var packLocationInfo = new PackLocationInfo(
+                    "builtin/dark_mode",
+                    Component.translatable("resourcePack.nemos_inventory_sorting.dark_mode.name"),
+                    PackSource.BUILT_IN,
+                    Optional.empty());
+            var pathResourcesSupplier = new PathPackResources.PathResourcesSupplier(resourcePath);
+            var packSelectionConfig = new PackSelectionConfig(false, Pack.Position.TOP, false);
+            var pack = Pack.readMetaAndCreate(packLocationInfo,
+                    pathResourcesSupplier,
+                    PackType.CLIENT_RESOURCES,
+                    packSelectionConfig);
+
+            event.addRepositorySource((packConsumer) -> packConsumer.accept(pack));
+        }
     }
 }
