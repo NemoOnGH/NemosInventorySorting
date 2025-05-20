bc134b3ff15ad59cb874d35f683dfb69c6cad74c
NemoNotFound <unconfigured@null.spigotmc.org>
2025-05-20 11:02:04 +0200

    Fix typing t in filter box while having recipe book open, start searching for recipes

--- common/src/main/java/com/nemonotfound/nemos/inventory/sorting/mixin/AbstractRecipeBookScreenMixin.java
+++ common/src/main/java/com/nemonotfound/nemos/inventory/sorting/mixin/AbstractRecipeBookScreenMixin.java
@@ -1,5 +1,6 @@
 package com.nemonotfound.nemos.inventory.sorting.mixin;
 
+import com.nemonotfound.nemos.inventory.sorting.client.gui.components.FilterBox;
 import com.nemonotfound.nemos.inventory.sorting.client.gui.components.RecipeBookUpdatable;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.client.gui.screens.inventory.AbstractRecipeBookScreen;
@@ -10,6 +11,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(AbstractRecipeBookScreen.class)
 public abstract class AbstractRecipeBookScreenMixin extends AbstractContainerScreen {
@@ -23,5 +25,16 @@ public abstract class AbstractRecipeBookScreenMixin extends AbstractContainerScr
         children().stream()
                 .filter(widget -> widget instanceof RecipeBookUpdatable)
                 .forEach(widget -> ((RecipeBookUpdatable) widget).updateXPosition(this.leftPos));
+    }
+
+    @Inject(method = "keyPressed", at = @At("HEAD"), cancellable = true)
+    private void keyPressed(int keyCode, int scanCode, int modifiers, CallbackInfoReturnable<Boolean> cir) {
+        var optionalFilterBox = children().stream()
+                .filter(widget -> widget instanceof FilterBox)
+                .findFirst();
+
+        if (optionalFilterBox.isPresent() && optionalFilterBox.get().isFocused()) {
+            cir.setReturnValue(super.keyPressed(keyCode, scanCode, modifiers));
+        }
     }
 }
